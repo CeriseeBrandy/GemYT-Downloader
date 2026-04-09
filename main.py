4817ee763d2951f8cedb3f34e6b9919b2ca607e1
Cerisee <cherrygaming0804@gmail.com>
2026-04-09 12:20:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class Splash(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        # COLORS
+        
         self.bg_color = "#242424"
         self.theme_color = "#FF3B3B"
 
         self.geometry("450x400")
         self.overrideredirect(True)
 
-        # CENTER
+        
         self.update_idletasks()
         w, h = 450, 400
         x = (self.winfo_screenwidth() // 2) - (w // 2)
@@ -26,18 +26,18 @@ class Splash(ctk.CTk):
 
         self.configure(fg_color=self.bg_color)
 
-        # animation vars
+        
         self.scale = 1.0
         self.direction = 1
         self.alpha = 0
 
-        # LOGO
+        
         self.base_img = Image.open("assets/ruby.png")
 
         self.logo = ctk.CTkLabel(self, text="")
         self.logo.pack(pady=40)
 
-        # TITLE (simple rouge)
+       
         self.full_text = "RubyYT Downloader"
         self.current_text = ""
 
@@ -49,7 +49,7 @@ class Splash(ctk.CTk):
         )
         self.title_label.pack(pady=5)
 
-        # SUBTEXT
+        
         self.subtitle = ctk.CTkLabel(
             self,
             text="Initializing...",
@@ -58,13 +58,13 @@ class Splash(ctk.CTk):
         )
         self.subtitle.pack()
 
-        # PROGRESS
+        
         self.progress = ctk.CTkProgressBar(self, width=250)
         self.progress.pack(pady=20)
         self.progress.set(0)
         self.progress.configure(progress_color=self.theme_color)
 
-        # START
+        
         self.attributes("-alpha", 0)
         self.fade_in()
 
@@ -72,14 +72,14 @@ class Splash(ctk.CTk):
         self.animate_progress()
         self.type_text()
 
-    # ===== FADE IN =====
+    
     def fade_in(self):
         if self.alpha < 1:
             self.alpha += 0.03
             self.attributes("-alpha", self.alpha)
             self.after(20, self.fade_in)
 
-    # ===== FADE OUT =====
+    
     def fade_out(self):
         if self.alpha > 0:
             self.alpha -= 0.03
@@ -88,14 +88,14 @@ class Splash(ctk.CTk):
         else:
             self.launch_app()
 
-    # ===== TYPE TEXT =====
+    
     def type_text(self):
         if len(self.current_text) < len(self.full_text):
             self.current_text += self.full_text[len(self.current_text)]
             self.title_label.configure(text=self.current_text)
             self.after(40, self.type_text)
 
-    # ===== LOGO ANIMATION =====
+    
     def animate_logo(self):
         if self.scale >= 1.08:
             self.direction = -1
@@ -118,7 +118,7 @@ class Splash(ctk.CTk):
 
         self.after(30, self.animate_logo)
 
-    # ===== PROGRESS =====
+    
     def animate_progress(self):
         current = self.progress.get()
 
@@ -128,7 +128,7 @@ class Splash(ctk.CTk):
         else:
             self.after(400, self.fade_out)
 
-    # ===== LAUNCH =====
+    
     def launch_app(self):
         self.destroy()
         app = App()
